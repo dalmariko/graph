f81628c690dd815d74d89a6344c2ab93a6227086
dalmariko <dalmarikocamp@gmail.com>
2019-03-12 10:18:21 +0200

    todo make

--- chart.js
+++ chart.js
@@ -10,34 +10,46 @@ class Chart {
 
     static addTemplate(settings) {
         let template = Chart.template(settings);
-        document.querySelector(settings.chartContainer).insertAdjacentHTML('afterbegin', template);
+        document.querySelector(settings.chartsContainer).insertAdjacentHTML('beforeend', template);
     }
 
     static template(settings) {
-        return ` <svg height=${settings.height} width=${settings.width}>
+        return ` 
+ <div class="chart">
+ <svg height=${settings.height} width=${settings.width}>
    
     <polyline points="${settings.axisY0}" 
-  
+
   style="fill:${settings.fill};stroke:${settings.colors.y0};stroke-width:${settings.strokeWidth}" />
     <polyline points="${settings.axisY1}" 
+
+  style="fill:${settings.fill};stroke:${settings.colors.y1};stroke-width:${settings.strokeWidth}" />
+    <polyline points="${settings.axisY2}" 
+  
+  style="fill:${settings.fill};stroke:${settings.colors.y0};stroke-width:${settings.strokeWidth}" />
+    <polyline points="${settings.axisY3}" 
   
   style="fill:${settings.fill};stroke:${settings.colors.y1};stroke-width:${settings.strokeWidth}" />
   
-</svg> `;
+</svg>
+</div>
+`;
     }
 
     static getDefaultsettings() {
         /*
          *Default Settings
-         * chartContainer - insert all chart basic container
+         * chartsContainer - insert all chart basic container
          */
         return {
-            chartContainer: '.chart',
+            chartsContainer: '.manyCharts',
             width: '100%',
-            height: '300',
+            height: '500',
             fill: 'transparent',
             axisY0: '',
             axisY1: '',
+            axisY2: '',
+            axisY3: '',
             types: {"y0": "line", "y1": "line", "x": "x"},
             names: {"y0": "#0", "y1": "#1"},
             colors: {"y0": "#3DC23F", "y1": "#F34C44"},
@@ -49,9 +61,12 @@ class Chart {
 
 
 
-let points={Y0:'',Y1:''};
 
-const getDate=(data)=>{
+
+
+const makeChart=(data)=>{
+    let points={Y0:'',Y1:'',Y2:'',Y3:''};
+
     let axis = 1;
     let ctn = data.columns[0].length;
     let x=0;
@@ -65,23 +80,29 @@ const getDate=(data)=>{
         let y0 = data.columns[1][axis];
         let y1 = data.columns[2][axis];
 
-        points.Y0+=`${(x*day+axis)*7.8},${y0} `;
-        points.Y1+=`${(x*day+axis)*7.8},${y1} `;
+        points.Y0+=`${(x*day+axis)*5},${y0*0.01} `;
+        points.Y1+=`${(x*day+axis)*5},${y1*0.01} `;
 
         axis++;
     }
 
-    return points;
+
+    return new Chart({
+        axisY0: points.Y0,
+        axisY1: points.Y1,
+        axisY2: points.Y2,
+        axisY3: points.Y3
+    }).init();
 };
 
 data.forEach(item=>{
-    getDate(item);
+    makeChart(item);
 });
 
 
 
 
-const myChart = new Chart({
-    axisY0: points.Y0,
-    axisY1: points.Y1
-}).init();
+
+
+//todo Найти максимумы для осей с помощью сотрировки и привязать этот максимум к размеру экрана
+
