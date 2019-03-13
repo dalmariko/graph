b15bddd575e74cc1a7cc80f56665c957c72bf08a
dalmariko <dalmarikocamp@gmail.com>
2019-03-13 02:36:26 +0200

    all graphics bild

--- chart.js
+++ chart.js
@@ -77,10 +77,11 @@ const getExtremum=(dataY)=>{
 };
 
 const makeChart=(data)=>{
-    let points={};
+    let points={Y0:'',Y1:'',Y2:'',Y3:''};
 
     let axis = 1;
     let ctn = data.columns[0].length;
+
 
     let oneYear=31557600000;
     let oneDay=86400000;
@@ -88,32 +89,25 @@ const makeChart=(data)=>{
     let scales=getScale(data);
     let scaleX=scales.scaleX;
     let scaleY='';
-
-    let x='0';
-    let  y='0';
+    let x=0;
+    let y=0;
 
     while (axis < ctn) {
-
         // let year= data.columns[0][axis]/oneYear^0;
         // let day =(data.columns[0][axis]-year*oneYear)/oneDay^0;
-
         // x = parseFloat( ((0 * day + axis) * scaleX).toFixed(4) );
 
-            x = parseFloat((axis * scaleX).toFixed(4));
+        x = parseFloat(( (axis) * scaleX).toFixed(4));
 
             for (let i = 1; i < data.columns.length; i++) {
-                scaleY = scales.scaleY / getExtremum(data.columns[i]);
 
+                scaleY = scales.scaleY / getExtremum(data.columns[i]);
                 y = parseFloat((data.columns[i][axis] * scaleY).toFixed(4));
 
-                points[`Y${i-1}`] += `${x},${y} `;
+                points[`Y${i-1}`]+=`${x},${y} `;
             }
-
             axis++;
-    }
-
-console.log(points)
-
+        }
 
     return new Chart({
         axis: points,
@@ -121,7 +115,9 @@ console.log(points)
     }).init();
 };
 
-makeChart(data[0]);
+data.forEach(item=>{
+    makeChart(item);
+});
 
 
 
